69f9c7671ca565dfed8a9cd925322df94ff3a1d5
Georgy <geovo851@gmail.com>
2016-01-06 06:09:57 -0800

    Added index

--- config/application.rb
+++ config/application.rb
@@ -31,6 +31,7 @@ module ElectronicsStore
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    config.assets.paths << Rails.root.join("app", "assets", "fonts")
     config.generators do |g|
       g.assets = false
       g.helper = false
